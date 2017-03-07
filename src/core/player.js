418da6b62705884d050ea67443915ab6d63b3046
kevin940726 <kevin830726@gmail.com>
2017-03-07 22:01:30 +0800

    🔨 Refactor: create a wrapper for createPlayer

--- src/core/player.js
+++ src/core/player.js
@@ -1,17 +1,25 @@
+const createPlayer = ({
+  play = () => {},
+  pause = () => {},
+}) => ({
+  play,
+  pause,
+});
+
 function setPlayer(player, type) {
   if (type === 'youtube') {
-    return {
+    return createPlayer({
       play: () => player.playVideo(),
       pause: () => player.pauseVideo(),
-    };
+    });
   } else if (type === 'soundcloud') {
-    return {
+    return createPlayer({
       play: () => player.play(),
       pause: () => player.pause(),
-    };
+    });
   }
 
-  return {};
+  return createPlayer({});
 }
 
 export default setPlayer;
